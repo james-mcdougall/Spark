d893cba698f0745a6b49228b44fc6378c71f5f17
james.mcdougall <jameswilliammcdougall@gmail.com>
2023-02-10 16:35:22 -0600

    Created CreationDetailView

---
+++ Spark/Spark/Screens/CreationView.swift
@@ -0,0 +1,20 @@
+//
+//  CreationView.swift
+//  Spark
+//
+//  Created by James McDougall on 2/10/23.
+//
+
+import SwiftUI
+
+struct CreationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreationView()
+    }
+}
