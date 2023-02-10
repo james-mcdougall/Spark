ade7ab28701004d48c54a28d2c8f02aa753d18ab
james-mcdougall <jameswilliammcdougall@gmail.com>
2023-02-10 15:58:56 -0600

    Imitial Project Commit

---
+++ Spark/Spark/SparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkApp.swift
+//  Spark
+//
+//  Created by James McDougall on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
